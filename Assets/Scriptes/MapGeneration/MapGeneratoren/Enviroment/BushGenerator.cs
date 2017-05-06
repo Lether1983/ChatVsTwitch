d42db1d2b2210a1325b555e8edbe15acd12fc1dc
Vincent Bendigs <VincentBendigs@googlemail.com>
2017-05-07 01:02:20 +0200

    MODIFY: The BushGenerator

--- Assets/Scriptes/MapGeneration/MapGeneratoren/Enviroment/BushGenerator.cs
+++ Assets/Scriptes/MapGeneration/MapGeneratoren/Enviroment/BushGenerator.cs
@@ -11,16 +11,31 @@ public class BushGenerator : IMapGenerator
 
     public int[,] Generate()
     {
-        return null;
+        SpawnBushesOnMap();
+        return RandomMap;
     }
 
     public void Setup(int width, int height, int[,] Randommap)
     {
-
+        Mapwidth = width;
+        Mapheight = height;
+        RandomMap = Randommap;
     }
 
     private void SpawnBushesOnMap()
     {
 
+        int tempX = UnityEngine.Random.Range(0, Mapwidth);
+        int tempY = UnityEngine.Random.Range(0, Mapheight);
+        for (int x = 0; x < Mapwidth; x++)
+        {
+            if (tempX < 50 || tempX > 70)
+            {
+                if (RandomMap[tempX, tempY] != 0)
+                {
+                       
+                }
+            }
+        }
     }
 }
