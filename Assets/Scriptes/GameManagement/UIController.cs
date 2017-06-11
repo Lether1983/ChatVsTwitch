367ab6c80e998426b8824244c1a3ddbbadbf3127
Vincent Bendigs <VincentBendigs@googlemail.com>
2017-06-12 00:35:11 +0200

    ADDED: A New Folder and a UI Controller

---
+++ Assets/Scriptes/GameManagement/UIController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject VoteShowObject;
+
+    public void ActivateVote()
+    {
+
+    }
+}
